b73413f31ddef83e6a3dbb4810fc27ab402d5c58
Mike Blaszczak <mikeblas@gmail.com>
2023-12-22 19:00:21 -0800

    days 14 and 15

---
+++ Day14/Program.cs
@@ -0,0 +1,80 @@
+﻿namespace Day14
+{
+    internal class Program
+    {
+        static void PaintRocksNorth(char [,] spots, int x, int start, int end, int count)
+        {
+            for (int y = end; y < end+count; y++)
+                spots[y,x] = 'O';
+            for (int y = end + count; y <= start; y++)
+                spots[y,x] = '.';
+        }
+
+        static void MoveNorth(char[,] spots)
+        {
+            for (int x = 0; x < spots.GetLength(1); x++)
+            {
+                int startY = -1;
+                int rocks = 0;
+                for (int y = spots.GetLength(0) - 1; y >= -1; y--)
+                {
+                    if (y == -1 || spots[y, x] == '#')
+                    {
+                        if (startY != -1)
+                        {
+                            PaintRocksNorth(spots, x, startY, y + 1, rocks);
+                            startY = -1;
+                            rocks = 0;
+                        }
+                    }
+                    else
+                    {
+                        if (startY == -1)
+                            startY = y;
+                        if (spots[y, x] == 'O')
+                        {
+                            rocks += 1;
+                        }
+                    }
+                }
+            }
+        }
+
+
+        static void Part1(string fileName)
+        {
+            string[] lines = File.ReadAllLines(fileName);
+            char[,] spots = new char[lines.Length, lines[0].Length];
+            for (int y = 0; y < lines.Length; y++)
+                for (int x = 0; x < lines[y].Length; x++)
+                    spots[y, x] = lines[y][x];
+
+            MoveNorth(spots);
+
+            int weight = 0;
+            for (int y = 0; y < lines.Length; y++)
+            {
+                int rocks = 0;
+
+                for (int x = 0; x < lines[y].Length; x++)
+                {
+                    Console.Write(spots[y, x]);
+                    if (spots[y, x] == 'O')
+                        rocks++;
+                }
+                Console.WriteLine($"  {lines.Length - y}");
+
+                weight += rocks * (lines.Length - y);
+            }
+
+            Console.WriteLine($"{fileName} has weight {weight}");
+        }
+
+
+        static void Main(string[] args)
+        {
+            Part1("sample.txt");
+            Part1("part1.txt");
+        }
+    }
+}
---
+++ Day15/Program.cs
@@ -0,0 +1,148 @@
+﻿using System.Diagnostics;
+using System.Transactions;
+
+namespace Day15
+{
+    internal class Lens
+    {
+
+        internal Lens(int focalLength, string lensName)
+        {
+            this.FocalLength = focalLength;
+            this.LensName = lensName;
+        }
+
+        internal string LensName { get; set; }
+        internal int FocalLength { get; set; }
+    }
+
+
+    internal class Program
+    {
+
+        static void Part2Multi(string str)
+        {
+            List<List<Lens>> boxes = [];
+            for (int i = 0; i < 256; i++)
+                boxes.Add([]);
+
+            string[] parts = str.Split(',');
+            foreach (string part in parts)
+            {
+                int eq = part.IndexOf('=');
+                int minus = part.IndexOf("-");
+                if (eq != -1)
+                {
+                    // equals
+                    string box = part.Substring(0, eq);
+                    string right = part.Substring(eq + 1);
+                    int lensLength = int.Parse(right);
+                    int n = Part1(box);
+
+                    int found = -1;
+                    for (int i = 0; i < boxes[n].Count; i++)
+                    {
+                        if (boxes[n][i].LensName.Equals(box))
+                        {
+                            found = i;
+                            break;
+                        }
+                    }
+
+                    if (found == -1)
+                        boxes[n].Add(new Lens(lensLength, box));
+                    else
+                    {
+                        boxes[n][found].FocalLength = lensLength;
+                    }
+                }
+                else if (minus != -1)
+                {
+                    string box = part.Substring(0, minus);
+                    int n = Part1(box);
+
+                    int found = -1;
+                    for (int i = 0; i < boxes[n].Count; i++)
+                    {
+                        if (boxes[n][i].LensName.Equals(box))
+                        {
+                            found = i;
+                            break;
+                        }
+                    }
+
+                    if (found != -1)
+                        boxes[n].RemoveAt(found);
+
+                }
+                else
+                    Debug.Assert(false);
+            }
+
+            int sum = 0;
+            for (int i = 0; i < boxes.Count; i++)
+            {
+                Console.Write($"Box {i}: ");
+                for (int slot = 0; slot < boxes[i].Count; slot++)
+                {
+                    sum += (i + 1) * (slot + 1) * boxes[i][slot].FocalLength;
+                    Console.Write($"[{boxes[i][slot].LensName} {boxes[i][slot].FocalLength}] ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine($"sum is {sum}");
+        }
+
+        static void Part2File(string fileName)
+        {
+            string[] lines = File.ReadAllLines(fileName);
+            Part2Multi(lines[0]);
+        }
+
+
+        static void Part1File(string fileName)
+        {
+            string[] lines = File.ReadAllLines(fileName);
+            Part1Multi(lines[0]);
+        }
+
+        static void Part1Multi(string str)
+        {
+            int sum = 0;
+            string[] parts = str.Split(',');
+            foreach (string part in parts)
+            {
+                sum += Part1(part);
+            }
+
+            Console.WriteLine(sum);
+        }
+
+
+        static int Part1(string str)
+        {
+            int current = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                current += str[i];
+                current *= 17;
+                current = current % 256;
+            }
+
+            // Console.WriteLine($"result is {current}");
+            return current;
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Hash test is {Part1("HASH")}");
+            Part1Multi("rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7");
+            Part1File("part1.txt");
+
+            Part2Multi("rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7");
+            Part2File("part1.txt");
+        }
+    }
+}
